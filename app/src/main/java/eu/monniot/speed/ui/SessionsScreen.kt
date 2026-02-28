a17c586c499528996d9abbfc44524830154b79b0
François Monniot <francoismonniot@gmail.com>
2026-02-28 08:13:27 -0800

    delete action should use a delete icon

--- app/src/main/java/eu/monniot/speed/ui/SessionsScreen.kt
+++ app/src/main/java/eu/monniot/speed/ui/SessionsScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -108,7 +108,7 @@ fun SessionItem(
                 )
             }
             IconButton(onClick = { onDelete(session.sessionId) }) {
-                Icon(Icons.Default.PlayArrow, contentDescription = "Details", tint = Color.Gray)
+                Icon(Icons.Default.Delete, contentDescription = "Delete", tint = Color.Gray)
             }
         }
     }
